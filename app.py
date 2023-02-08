1e3fa7397025eae362a8d14fbad8807225126867
Bhushan Zade <bhushanzade.in@gmail.com>
2023-02-09 00:36:37 +0530

    commit

--- app.py
+++ app.py
@@ -1,77 +1,189 @@
 import streamlit as st
-from io import BytesIO
-from PIL import Image
-import numpy as np
+import os
+import tensorflow_hub as hub
+from myfunctions import load_img, transform_img, tensor_to_image, imshow, export_image
 import tensorflow as tf
-import tensorflow_hub as tf_hub
-import matplotlib.pyplot as plt
 
-tf.executing_eagerly()
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.set_page_config(
-    page_title="Neural Style Transfer", layout="wide"
-)
 
-def load_image(image_buffer, image_size=(512, 256)):
-    img = plt.imread(image_buffer).astype(np.float32)[np.newaxis, ...]
-    if img.max() > 1.0:
-      img = img / 255.
-    if len(img.shape) == 3:
-      img = tf.stack([img, img, img], axis=-1)
-    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
-    return img
 
-def export_image(tf_img):
-	pil_image = Image.fromarray(np.squeeze(tf_img*255).astype(np.uint8))
-	buffer = BytesIO()
-	pil_image.save(buffer, format="PNG")
-	byte_image = buffer.getvalue()
-	return byte_image
 
-def st_ui():
-    image_upload1 = st.sidebar.file_uploader("Load your image 1 here",type=["jpeg", "png", "jpg"], accept_multiple_files=False, key=None, help="Upload the image whom you want to style")
-    image_upload2 = st.sidebar.file_uploader("Load your image 2 here",type=["jpeg", "png", "jpg"], accept_multiple_files=False, key=None, help="Upload the image whose style you want")
-    col1,col2,col3= st.columns(3)
-    
-    st.sidebar.title("Style Transfer")
-    st.sidebar.markdown("Your personal neural style transfer")
+# Only use the below code if have low resources.
+os.environ['CUDA_VISIBLE_DEVICES'] = ""
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
 
-    with st.spinner("Loading content image.."):
-        if image_upload1 is not None:
-            col1.header("Content Image")
-            col1.image(image_upload1,use_column_width=True)
-            original_image = load_image(image_upload1)
-        else:
-            original_image = load_image("1.jpg")
-    
-    with st.spinner("Loading style image.."):
-        if image_upload2 is not None:
-            col2.header("Style Image")
-            col2.image(image_upload2,use_column_width=True)
-            style_image = load_image(image_upload2)
-            style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='VALID')
-        else:
-            style_image = load_image("2.jpg")
-            style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='VALID')
-    
+# For supressing warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-    if st.sidebar.button(label="Start Styling"):
-        if image_upload2 and image_upload1:
-            with st.spinner('Generating Stylized image ...'):
+st.set_page_config(page_title="Neural Style Transfer-Bhushan", layout="wide")
+st.write("""
+# Neural Style Transfer
+""")
 
-                # Load image stylization module.
-                stylize_model = tf_hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+# Load Pretrained Model
+@st.cache
+def load_model():
+    model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+    return model
 
-                results = stylize_model(tf.constant(original_image), tf.constant(style_image))
-                stylized_photo = results[0]
-                col3.header("Final Image")
-                col3.image(np.array(stylized_photo))
-                st.download_button(label="Download Final Image", data=export_image(stylized_photo), file_name="stylized_image.png", mime="image/png")
 
-        else:
-            st.sidebar.markdown("Please upload images...")
-            
-            
-if __name__ == "__main__":
-    st_ui()
+model_load_state = st.text('Loading Model')
+model = load_model()
+# Notify that the data was successfully loaded.
+model_load_state.text('')
+
+
+content_image, style_image, final = st.columns(3)
+
+with content_image:
+    st.write('## Content Image')
+    chosen_content = st.sidebar.radio(
+        '  ',
+        ("Upload", "Camera", "URL"),)
+
+# 1.OPTION-> UPLOAD
+    if chosen_content == 'Upload':
+        st.write(f"You choosed {chosen_content}!")
+        content_image_file = st.sidebar.file_uploader(
+            "Pick a Content image", type=("png", "jpg", "jpeg"))
+        try:
+            content_image_file = content_image_file.read()
+            content_image_file = transform_img(content_image_file)
+        except:
+            pass
+
+
+# 2.OPTION-> CAMERA
+    elif chosen_content == 'Camera':
+        st.write(f"You choosed {chosen_content}!")
+        content_image_file = st.sidebar.camera_input("Take a picture")
+        try:
+            content_image_file = content_image_file.read()
+            content_image_file = transform_img(content_image_file)
+        except:
+            pass
+
+# 3.OPTION-> URL
+    elif chosen_content == 'URL':
+        st.write(f"You choosed {chosen_content}!")
+        url = st.sidebar.text_input('URL for the content image.')
+
+        try:
+            content_path = tf.keras.utils.get_file(os.path.join(os.getcwd(), 'content.jpg'), url)
+        except:
+            pass
+
+        try:
+            content_image_file = load_img(content_path)
+        except:
+            pass
+
+# SHOW CONTENT IMAGE
+    try:
+        st.write('Content Image')
+        st.image(imshow(content_image_file))
+    except:
+        pass
+
+
+# STYLE IMAGE
+with style_image:
+    st.write('## Style Image')
+    chosen_style = st.sidebar.radio(
+        ' ',
+        ("Upload", "URL"))
+
+# 1.OPTION-> UPLOAD
+    if chosen_style == 'Upload':
+        st.write(f"You choosed {chosen_style}!")
+        style_image_file = st.sidebar.file_uploader(
+            "Pick a Style image", type=("png", "jpg", "jpeg"))
+        try:
+            style_image_file = style_image_file.read()
+            style_image_file = transform_img(style_image_file)
+        except:
+            pass
+
+# 2.OPTION-> URL
+    elif chosen_style == 'URL':
+        st.write(f"You choosed {chosen_style}!")
+        url = st.sidebar.text_input('URL for the style image.')
+        try:
+            style_path = tf.keras.utils.get_file(
+                os.path.join(os.getcwd(), 'style.jpg'), url)
+        except:
+            pass
+        try:
+            style_image_file = load_img(style_path)
+
+        except:
+            pass
+
+# SHOW STYLE IMAGE
+    try:
+        st.write('Style Image')
+        st.image(imshow(style_image_file))
+    except:
+        pass
+
+
+
+
+
+with final:
+    # FINAL PREDICTION-> NEURAL STYLE TRANSFORMER
+    button_style = """
+            <style>
+            .stButton > button {
+                color: black;
+                background: white;
+                border: 3px solid;
+                border-radius: 10px;
+                font-size: 150px;
+                width: 200px;
+                height: 75px;
+            }
+            </style>
+            """
+    st.markdown(button_style, unsafe_allow_html=True)
+
+    button_style = """
+                <style>
+                .stButton > download_button {
+                    color: black;
+                    background: white;
+                    border: 3px solid;
+                    border-radius: 10px;
+                    font-size: 150px;
+                    width: 250px;
+                    height: 100px;
+                }
+                </style>
+                """
+    st.markdown(button_style, unsafe_allow_html=True)
+
+    predict = st.button("***Start Neural Style Transfer***")
+    with st.spinner("Processing..."):
+        if predict:
+            if content_image_file is not None and style_image_file is not None:
+                try:
+                    stylized_image = model(tf.constant(content_image_file), tf.constant(style_image_file))[0]
+                    final_image = tensor_to_image(stylized_image)
+                except:
+                    stylized_image = model(tf.constant(tf.convert_to_tensor(content_image_file[:, :, :, :3])),tf.constant(tf.convert_to_tensor(style_image_file[:, :, :, :3])))[0]
+                    final_image = tensor_to_image(stylized_image)
+
+                a= st.write('## Final Image')
+                st.image(final_image)
+                st.download_button(label="**Download Final Image**", data=export_image(stylized_image), file_name="final_image.png",
+                                   mime="image/png")
+
+                try:
+                    # Delete style.jpg and content.jpg
+                    os.remove("style.jpg")
+                    os.remove("content.jpg")
+                except:
+                    pass
+            else:
+                st.markdown("### Please Upload Both Images")
+
 
---
+++ myfunctions.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import numpy as np
+from io import BytesIO
+from PIL import Image
+
+
+
+# Functions
+
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+
+def transform_img(img):
+    max_dim = 512
+    img = tf.image.decode_image(img)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+
+def imshow(image, title=None):
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+
+    image = np.squeeze(image)
+    return image
+
+
+def tensor_to_image(tensor):
+    tensor = tensor * 255
+    tensor = np.array(tensor, np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return Image.fromarray(tensor)
+
+
+def export_image(tf_img):
+	pil_image = Image.fromarray(np.squeeze(tf_img*255).astype(np.uint8))
+	buffer = BytesIO()
+	pil_image.save(buffer, format="PNG")
+	byte_image = buffer.getvalue()
+	return byte_image
